a5b40cabf9203a1e6f0641bc765b7363711d2470
IlTuoNome <laTuaEmail@example.com>
2025-03-12 15:15:32 +0100

    Add  Express server setup with root route

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ Configuriamo gli asset statici sull’applicazione in modo che si possano visual
 Testare su postman
 
 */
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () =>{
+    console.log(`Server is running on port http://localhost:${port}`);
+    
+})
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
